aa9f3ea10b480a6f9c6eca6677e7f6e69d964844
XS_Jinbert <mr_jinbert@163.com>
2020-05-02 17:00:55 +0800

    添加项目文件。

---
+++ WPFLearning/MyLearningContral/ButtomClick.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLearning
+{
+    /// <summary>
+    /// ButtomClick.xaml 的交互逻辑
+    /// </summary>
+    public partial class ButtomClick : UserControl
+    {
+        public ButtomClick()
+        {
+            InitializeComponent();
+        }
+
+        private void buttom1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I just a buttom");
+        }
+    }
+}
